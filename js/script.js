ac958687feb11d906dece30336da86d9bc42d856
hxasfarhani <hxasfh@gmail.com>
2022-06-20 14:33:58 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.scroll').on('click', function(e){
+
+	var tujuan = $('this').attr('href');
+
+	var elemenTujuan = $(tujuan);
+
+	$('body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	});
+
+	e.preventDefault();
+});
